d19ecb3d2fad2779ee4aecad3fe487fedfcbed8b
ncbjr <32766786+ncbjr@users.noreply.github.com>
2023-03-23 21:12:23 -0300

    Add files via upload

--- Aula 4/main.py
+++ Aula 4/main.py
@@ -1,18 +1,20 @@
+from cliente import Cliente
 from ClassConta import *
 
 if __name__ == '__main__':    
-    conta = Conta('123-5','Nilton Junior',100,5000)
-    conta2 = Conta('124-5','Nilton Braga',80,5000)
-    print(conta.saldo)
-    print(conta2.saldo)
-    conta.Transfere(conta2,50)
-    print(conta.saldo)
-    print(conta2.saldo)
+    cliente1 = Cliente('Nilton','Junior','17754327775')
+    conta1 = Conta('123-5',cliente1,100,5000)
+    
+    cliente2 = Cliente('Marcos','Antônio','18616111919')
+    conta2 = Conta('133-5',cliente2,500,5000)
+    
+    for conta in [ conta1, conta2 ]:
+        print("\nExtrato da conta de {}".format(conta.titular.nome))
+        print("Saldo atual: {}\n".format(conta.saldo))
+        conta.historico.imprime()
 
-"""
-    conta.Exibir()
-    conta.Deposita(100)
-    conta.Extrato()
-    conta.Saque(300)
-    conta.Extrato()
-"""
+    conta1.Transfere(conta2,50)
+
+    for conta in [ conta1, conta2 ]:
+        print()
+        conta.historico.imprime()
