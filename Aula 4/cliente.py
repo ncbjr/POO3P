a9deafd4c4c17d599df72390c305fa7b5d8ea63a
ncbjr <32766786+ncbjr@users.noreply.github.com>
2023-03-23 21:11:30 -0300

    Add files via upload

---
+++ Aula 4/cliente.py
@@ -0,0 +1,7 @@
+class Cliente:
+    def __init__(self, nome, sobrenome, cpf):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.cpf = cpf
+    def exibir(self):
+        print(f'Nome: {self.nome}\nSobrenome: {self.sobrenome}')
