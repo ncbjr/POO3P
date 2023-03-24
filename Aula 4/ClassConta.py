bfa84ad91af5573ca4f1378e9b2cf8407c41a6b5
ncbjr <32766786+ncbjr@users.noreply.github.com>
2023-03-23 21:10:37 -0300

    add histórico (extrato)

--- Aula 4/ClassConta.py
+++ Aula 4/ClassConta.py
@@ -1,21 +1,25 @@
+from historico import Historico
+
 class Conta:
     def __init__(self,cod,titular,saldo,limite):
         self.cod = cod
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
-        print('Nome do escopo',__name__)
-    
+        self.historico = Historico()
+
     def Exibir(self):
         print(f'Conta: {self.cod}\nTitular: {self.titular}\nSaldo: {self.saldo}\nLimite: {self.limite}')
     
     def Deposita(self,valor):
         self.saldo += valor
+        self.historico.transacoes.append("depósito de {}".format(valor))
     
     def Saque(self,valor):
         if self.saldo >= valor:
             self.saldo -= valor
             print(f'O seu novo saldo é: {self.saldo}')
+            self.historico.transacoes.append("saque de {}".format(valor))
             return True
         else:
             print(f'Não vai rolar! Seu saldo é: {self.saldo}')
@@ -26,4 +30,7 @@ class Conta:
     def Transfere(self,destino,valor):
         if self.Saque(valor):
             destino.Deposita(valor)
+            self.historico.transacoes.append("tranferência de {} para {} no valor de: {}".format(self.titular.nome, destino.titular.nome,valor))
+            destino.historico.transacoes.append("tranferência de {} para {} no valor de: {}".format(self.titular.nome, destino.titular.nome,valor))
+
         print(self.saldo)
