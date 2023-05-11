04de11222c4cf64689e081e17b53ed002c969b75
ncbjr <32766786+ncbjr@users.noreply.github.com>
2023-05-11 23:31:05 +0000

    cria mecânica do bônus anual

--- Aula 6?/Pessoas.py
+++ Aula 6?/Pessoas.py
@@ -1,26 +1,36 @@
+#Classe mãe
 class Pessoa():
     def __init__(self, nome, idade, cpf):
         self.nome = nome
         self.idade = idade
         self.cpf = cpf
 
+#Classe filha
 class Cliente(Pessoa):
     def __init__(self, nome, idade, cpf):
-        pass
-
-class Funcionario(Pessoa):
-    def __init__(self, nome, idade, cpf, salario):
         super().__init__(nome, idade, cpf)
-        self.salario = salario
 
+#Classe filha
+class Funcionario(Pessoa):
+    def __init__(self, nome, idade, cpf, salario,bonus=0):
+        super().__init__(nome, idade, cpf)
+        self.bonus = bonus
+        self.salario = salario
+    
+    def calculaBonus(self):
+        return(self.salario*self.bonus)
+
+#Classe neta
 class Gerente(Funcionario):
     def __init__(self,nome,idade,cpf,salario):
-        super().__init__(nome,idade,cpf,salario)
+        super().__init__(nome,idade,cpf,salario,bonus=0.9)
 
+#Classe neta
 class Coordenador(Funcionario):
     def __init__(self,nome,idade,cpf,salario):
-        super().__init__(nome,idade,cpf,salario)
+        super().__init__(nome,idade,cpf,salario,bonus=0.5)
 
+#Classe neta
 class Analista(Funcionario):
     def __init__(self,nome,idade,cpf,salario):
-        super().__init__(nome,idade,cpf,salario)
+        super().__init__(nome,idade,cpf,salario,bonus=0.2)
